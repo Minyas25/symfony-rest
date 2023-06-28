969c16b6eb9ec4e7437830e32b9b07faa6422ecf
Jean Demel <jean@m2i.fr>
2023-06-28 10:26:18 +0200

    partial test

--- tests/MovieApiTest.php
+++ tests/MovieApiTest.php
@@ -17,14 +17,47 @@ class MovieApiTest extends WebTestCase
         $this->assertNotEmpty($json);
         $this->assertIsString($json[0]['title']);
         $this->assertIsInt($json[0]['id']);
-
-
-        //exemple de post (à dégager, c'est juste pour l'exemple)
-        // $client->request('POST', '/api/movie', content: json_encode([
-        //     'title' => 'Mon titre',
-        // ]));
        
     }
 
 
+    public function testGetOneSuccess(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/movie/1');
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertIsString($json['title']);
+        $this->assertIsString($json['resume']);
+        $this->assertIsInt($json['duration']);
+        $this->assertIsString($json['released']);
+        $this->assertIsInt($json['id']);
+       
+    }
+    public function testGetOneNotFound(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/movie/100');
+        
+        $this->assertResponseStatusCodeSame(404);
+
+    }
+    
+    public function testPostSuccess(): void
+    {
+        $client = static::createClient();
+        $client->request('POST', '/api/movie', content: json_encode([
+            'title' => 'From Test',
+            'resume' => 'Resume Test',
+            'released' => '2020-10-01',
+            'duration' => 100
+        ]));
+        $json = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertIsInt($json['id']);
+    }
 }
